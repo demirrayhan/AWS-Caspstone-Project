6586ed5a8fe103816f8d31f370f1297c6351c73a
Ayhan Demir <demirrayhan@gmail.com>
2021-05-08 12:46:29 -0400

    updated files

--- userdata.sh
+++ userdata.sh
@@ -3,13 +3,13 @@ apt-get update -y
 apt-get install git -y
 apt-get install python3 -y
 cd /home/ubuntu/
-TOKEN=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-git clone https://$TOKEN@<YOUR GITHUB REPO URL>
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>
+TOKEN="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"
+git clone https://github.com/demirrayhan/AWS-Caspstone-Project.git
+cd /home/ubuntu/AWS-Caspstone-Project
 apt install python3-pip -y
 apt-get install python3.7-dev libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/<YOUR GITHUB REPO NAME>/src
+cd /home/ubuntu/AWS-Caspstone-Project/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
